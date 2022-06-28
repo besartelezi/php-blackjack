6aa8d662bc70851e6673a095336bebe54475a43d
Besart Elezi <besart.elezi@hotmail.com>
2022-06-28 16:13:02 +0200

    update index, updated sessions

--- code/index.php
+++ code/index.php
@@ -9,12 +9,52 @@ require 'Player.php';
 require 'Blackjack.php';
 
 session_start();
-$newGame = new Blackjack();
-$_SESSION['sample'] = serialize($newGame);
+if(isset($_SESSION['newGame']))
+{
+    $newGame = unserialize($_SESSION['newGame']);
+}
+else
+{
+    $newGame = new Blackjack();
+    $_SESSION['newGame'] = serialize($newGame);
+}
+
+$deck = new Deck();
+$deck->shuffle();
+foreach($deck->getCards() AS $card) {
+    echo $card->getUnicodeCharacter(true);
+    echo '<br>';
+}
+
+echo $newGame->getPlayer()->hasLost() ? 'true' : 'false';
+
+if (isset($_POST['hit']))
+{
+    $newGame->getPlayer()->hit($newGame->getDeck());
+}
+
+if (isset($_SESSION))
+{
+    echo "<br>";
+    echo "The Player's score is: ";
+    echo $newGame->getPlayer()->getScore();
+    echo "<br>";
+    echo "The Dealer's score is: ";
+    echo $newGame->getDealer()->getScore();
+}
+
+if (isset($_POST))
+if ($newGame->getPlayer()->hasLost() === true) {
+    echo "loser";
+}
 
 ?>
 
-<form action ="Player.php" method="get">
+
+
+
+
+<form action ="index.php" method="post">
     <button type="submit" name ="hit" value ="hit">Hit</button>
     <button type="submit" name ="stand" value ="stand">Stand</button>
     <button type="submit" name ="surrender" value ="surrender">Surrender</button>
